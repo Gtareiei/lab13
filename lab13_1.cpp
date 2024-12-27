e232d5d5d6ea2284492299e8ee869b076487f1fc
Gtareiei <Natthapong0848@gmail.com>
2024-12-27 22:55:57 +0700

    lab13_1complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double x[], int N, double y[]) {
+    double sum = 0;
+    double sum_sq = 0;
+    double product = 1;
+    double harmonic_sum = 0;
+    double max = x[0];
+    double min = x[0];
+
+    for (int i = 0; i < N; i++) {
+        sum += x[i];
+        sum_sq += x[i] * x[i];
+        product *= x[i];
+        harmonic_sum += 1.0 / x[i];
+        if (x[i] > max) max = x[i];
+        if (x[i] < min) min = x[i];
+    }
+
+    y[0] = sum / N;
+    double mean_sq = sum_sq / N;
+    y[1] = sqrt(mean_sq - (y[0] * y[0]));
+    y[2] = pow(product, 1.0 / N);
+    y[3] = N / harmonic_sum;
+    y[4] = max;
+    y[5] = min;
+}
